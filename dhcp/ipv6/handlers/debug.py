dcab2503f795659c7f7c806651b4ea6a3ece16b5
Sander Steffann <sander@steffann.nl>
2015-07-05 17:28:41 +0200

    Basic debugger for incoming messages

--- dhcp/ipv6/handlers/debug.py
+++ dhcp/ipv6/handlers/debug.py
@@ -1,14 +1,19 @@
 import configparser
+import logging
 
 from dhcp.ipv6.handlers import Handler
-from dhcp.ipv6.messages import ClientServerMessage, AdvertiseMessage, Message
+from dhcp.ipv6.messages import Message
+
+logger = logging.getLogger(__name__)
 
 
 class DebugHandler(Handler):
-    # noinspection PyMethodMayBeStatic,PyUnusedLocal
-    def handle_solicit_message(self, request: ClientServerMessage, relay_messages: list,
-                               sender: tuple, receiver: tuple) -> (Message, tuple) or Message or None:
-        return AdvertiseMessage()
+    def handle(self, received_message: Message, sender: tuple, receiver: tuple):
+        # Print the incoming request
+        logger.debug("Received message from {} to {}, {}".format(sender[0], receiver[0], received_message))
+
+        # Not sending any response
+        return None
 
 
 def get_handler(config: configparser.ConfigParser):
