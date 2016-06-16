ad451dd6b9cc933756cf66c948868917e7436282
Sander Steffann <sander@steffann.nl>
2016-06-16 23:53:06 +0200

    Add utility function for IPv6 Global Unicast checking

--- dhcpkit/ipv6/utils.py
+++ dhcpkit/ipv6/utils.py
@@ -34,3 +34,16 @@ def prefix_overlaps_prefixes(prefix: IPv6Network, prefixes: [IPv6Network]) -> bo
             return True
 
     return False
+
+
+def is_global_unicast(address: IPv6Address) -> bool:
+    """
+    Check if an address is a global unicast address according to :rfc:`4291`.
+
+    :param address: The address to check
+    :return: Whether it is a global unicast address
+    """
+    return not (address == IPv6Address('::') or
+                address == IPv6Address('::1') or
+                address in IPv6Network('ff00::/8') or
+                address in IPv6Network('fe80::/10'))
