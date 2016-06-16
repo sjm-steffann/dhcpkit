dcf2b81fe772046693c23101667c8eaf5ae929ec
Sander Steffann <sander@steffann.nl>
2016-06-17 00:01:11 +0200

    Update main configuration

---
+++ dhcpkit/ipv6/server/config_datatypes.py
@@ -0,0 +1,15 @@
+"""
+Extra datatypes for the server configuration
+"""
+
+
+def number_of_workers(value: str) -> int:
+    """
+    The number of worker processes, must be 1 or more
+    :param value: The number of processes
+    :return: The validated number of processes
+    """
+    value = int(value)
+    if value < 1:
+        raise ValueError("Number of workers must be at least 1")
+    return value
--- dhcpkit/ipv6/server/config_elements.py
+++ dhcpkit/ipv6/server/config_elements.py
@@ -4,31 +4,20 @@ The basic configuration objects
 
 import grp
 import logging
+import pwd
 
-from pip.utils import cached_property
-
+from dhcpkit.common.server.config_elements import ConfigSection
 from dhcpkit.common.server.logging.config_elements import Logging
-from dhcpkit.ipv6.extensions.prefix_delegation import IAPDOption, IAPrefixOption
-from dhcpkit.ipv6.messages import RelayServerMessage, Message, SolicitMessage, AdvertiseMessage, RequestMessage, \
-    RenewMessage, RebindMessage, ReleaseMessage, InformationRequestMessage, DeclineMessage, ReplyMessage, ConfirmMessage
-from dhcpkit.ipv6.options import IANAOption, IATAOption, IAAddressOption, ClientIdOption, ServerIdOption, \
-    StatusCodeOption, STATUS_USEMULTICAST
-from dhcpkit.ipv6.server.config_action import Action, CannotRespondError, UseMulticastError
-from dhcpkit.ipv6.server.config_filter import Filter
-from dhcpkit.ipv6.server.transaction_bundle import TransactionBundle
+from dhcpkit.ipv6.server.message_handler import MessageHandler
 from dhcpkit.ipv6.server.utils import determine_local_duid
 
 logger = logging.getLogger(__name__)
 
 
-class MainConfig(Filter):
+class MainConfig(ConfigSection):
     """
     The top level configuration element
     """
-
-    # This is not really a filter, it matches everything
-    filter_description = 'root'
-    filter_condition = '*'
 
     def clean_config_section(self):
         """
@@ -45,7 +34,7 @@ class MainConfig(Filter):
         if not self._section.server_id:
             self._section.server_id = determine_local_duid()
 
-    @cached_property
+    @property
     def logging(self) -> Logging:
         """
         Shortcut to the logging configuration.
@@ -54,154 +43,36 @@ class MainConfig(Filter):
         """
         return self._section.logging
 
-    def match(self, bundle: TransactionBundle) -> bool:
+    @property
+    def user(self) -> pwd.struct_passwd:
         """
-        The config root matches every message.
+        Shortcut to the user configuration.
 
-        :param bundle: The transaction bundle
-        :return: True
+        :return: User configuration
         """
-        return True
+        return self._section.user
 
-    def get_actions(self, bundle: TransactionBundle) -> [Action]:
+    @property
+    def group(self) -> grp.struct_group:
         """
-        Get all actions that are going to be applied to the request in the bundle.
+        Shortcut to the group configuration.
 
-        :param bundle: The transaction bundle
-        :return: The list of actions to apply
+        :return: Group configuration
         """
+        return self._section.group
 
-        # Build the actions list
-        actions = []
-
-        # if self.allow_rapid_commit:
-        #     # Rapid commit happens as the first thing in the post() stage
-        #     self.actions.append(RapidCommitOptionHandler(self.rapid_commit_rejections))
-
-        # These are mandatory
-        # actions.append(ServerIdOptionHandler(duid=self.server_duid))
-        # actions.append(ClientIdOptionHandler())
-        # actions.append(InterfaceIdOptionHandler())
-
-        # Add the ones from the configuration
-        actions += super().get_actions(bundle)
-
-        # Add cleanup handlers so they run last in the handling phase
-        # actions.append(UnansweredIAOptionHandler())
-        # actions.append(UnansweredIAPDOptionHandler())
-
-        # Confirm/Release/Decline messages always need a status
-        # actions.append(ConfirmStatusOptionHandler())
-        # actions.append(ReleaseStatusOptionHandler())
-        # actions.append(DeclineStatusOptionHandler())
-
-        return actions
-
-    @staticmethod
-    def init_response(bundle: TransactionBundle):
+    def create_message_handler(self) -> MessageHandler:
         """
-        Create the message object in bundle.response
+        Create a message handler based on this configuration.
 
-        :param bundle: The transaction bundle
+        :return: The message handler
         """
-        # Start building the response
-        if isinstance(bundle.request, SolicitMessage):
-            bundle.response = AdvertiseMessage(bundle.request.transaction_id)
+        sub_filters = []
+        for filter_factory in self._section.filter_factories:
+            sub_filters.append(filter_factory())
 
-        elif isinstance(bundle.request, (RequestMessage, RenewMessage, RebindMessage,
-                                         ReleaseMessage, DeclineMessage, InformationRequestMessage)):
-            bundle.response = ReplyMessage(bundle.request.transaction_id)
+        sub_handlers = []
+        for handler_factory in self._section.handler_factories:
+            sub_handlers.append(handler_factory())
 
-        elif isinstance(bundle.request, ConfirmMessage):
-            # Receipt of Confirm Messages: If [...] there were no addresses in any of the IAs sent by the client, the
-            # server MUST NOT send a reply to the client.
-            found = False
-            for option in bundle.request.get_options_of_type((IANAOption, IATAOption, IAPDOption)):
-                if option.get_options_of_type((IAAddressOption, IAPrefixOption)):
-                    # Found an address or prefix option
-                    found = True
-                    break
-
-            if not found:
-                raise CannotRespondError
-
-            bundle.response = ReplyMessage(bundle.request.transaction_id)
-
-        else:
-            logger.warning("Do not know how to reply to {}".format(type(bundle.request).__name__))
-            raise CannotRespondError
-
-        # Build the plain chain of relay reply messages
-        bundle.create_outgoing_relay_messages()
-
-    def construct_use_multicast_reply(self, bundle: TransactionBundle):
-        """
-        Construct a message signalling to the client that they should have used multicast.
-
-        :param bundle: The transaction bundle containing the incoming request
-        :return: The proper answer to tell a client to use multicast
-        """
-        # Make sure we only tell this to requests that came in over multicast
-        if not bundle.received_over_multicast:
-            return None
-
-        return ReplyMessage(bundle.request.transaction_id, options=[
-            bundle.request.get_option_of_type(ClientIdOption),
-            ServerIdOption(duid=self._section.server_id),
-            StatusCodeOption(STATUS_USEMULTICAST, "You cannot send requests directly to this server, "
-                                                  "please use the proper multicast addresses")
-        ])
-
-    def handle(self, received_message: RelayServerMessage, received_over_multicast: bool,
-               marks: [str] = None) -> Message or None:
-        """
-        The main dispatcher for incoming messages.
-
-        :param received_message: The parsed incoming request
-        :param received_over_multicast: Whether the request was received over multicast
-        :param marks: Marks to add to the transaction bundle, usually set by the listener
-        :returns: The message to reply with
-        """
-
-        # Create the transaction
-        bundle = TransactionBundle(incoming_message=received_message,
-                                   received_over_multicast=received_over_multicast,
-                                   allow_rapid_commit=self._section.allow_rapid_commit)
-
-        if not bundle.request:
-            # Nothing to do...
-            return None
-
-        # Add the marks so the filters can take them into account
-        if marks:
-            for mark in marks:
-                bundle.add_mark(mark)
-
-        # Collect the actions
-        actions = self.get_actions(bundle)
-
-        try:
-            # Pre-process the request
-            for option_handler in actions:
-                option_handler.pre(bundle)
-
-            # Init the response
-            self.init_response(bundle)
-
-            # Process the request
-            for option_handler in actions:
-                option_handler.handle(bundle)
-
-            # Post-process the request
-            for option_handler in actions:
-                option_handler.post(bundle)
-
-        except CannotRespondError:
-            logger.debug("Cannot respond to this message: ignoring")
-            bundle.response = None
-
-        except UseMulticastError:
-            logger.debug("Unicast request received when multicast is required: informing client")
-            bundle.response = self.construct_use_multicast_reply(bundle)
-
-        return bundle.outgoing_message
+        return MessageHandler(self._section.server_id, sub_filters, sub_handlers, self._section.allow_rapid_commit)
