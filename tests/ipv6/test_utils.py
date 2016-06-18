1ad47e8bd2a3601605929dbe17a163cf96ba3f6f
Sander Steffann <sander@steffann.nl>
2016-06-18 23:16:44 +0200

    Remove unnecessary utils and fix testing accordingly

--- tests/ipv6/test_utils.py
+++ tests/ipv6/test_utils.py
@@ -1,28 +1,22 @@
 """
 Test the IPv6 utility functions
 """
-from ipaddress import IPv6Network, IPv6Address
 import unittest
+from ipaddress import IPv6Address
 
-from dhcpkit.ipv6.utils import address_in_prefixes, prefix_overlaps_prefixes
+from dhcpkit.ipv6.utils import is_global_unicast
 
 
 class IPv6UtilsTestCase(unittest.TestCase):
-    def test_address_in_prefixes(self):
-        prefixes = [IPv6Network('2001:db8::/48'), IPv6Network('2001:db8:1:2::/64')]
-        good_address = IPv6Address('2001:db8::1')
-        bad_address = IPv6Address('2001:db8:1::1')
-
-        self.assertTrue(address_in_prefixes(good_address, prefixes))
-        self.assertFalse(address_in_prefixes(bad_address, prefixes))
-
-    def test_prefix_overlaps_prefixes(self):
-        prefixes = [IPv6Network('2001:db8::/48'), IPv6Network('2001:db8:1:2::/64')]
-        good_prefix = IPv6Network('2001:db8::/64')
-        bad_prefix = IPv6Network('2001:db8:1::/64')
-
-        self.assertTrue(prefix_overlaps_prefixes(good_prefix, prefixes))
-        self.assertFalse(prefix_overlaps_prefixes(bad_prefix, prefixes))
+    def test_is_global_unicast(self):
+        self.assertTrue(is_global_unicast(IPv6Address('2001:db8::1')))
+        self.assertTrue(is_global_unicast(IPv6Address('fc00::1')))
+        self.assertTrue(is_global_unicast(IPv6Address('fd00::1')))
+        self.assertTrue(is_global_unicast(IPv6Address('dead::beef')))
+        self.assertFalse(is_global_unicast(IPv6Address('::')))
+        self.assertFalse(is_global_unicast(IPv6Address('::1')))
+        self.assertFalse(is_global_unicast(IPv6Address('fe80::1')))
+        self.assertFalse(is_global_unicast(IPv6Address('ff02::1')))
 
 
 if __name__ == '__main__':
